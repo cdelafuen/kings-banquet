b51e4245b3c190c79f4489d46e361b724864ac08
cdelafuen <christian@doofinder.com>
2018-01-05 11:55:27 +0100

    Added example test

--- game/tests.py
+++ game/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
 # Create your tests here.
+from game.models import Game
+
+
+class GameTests(TestCase):
+
+    def setUp(self):
+        self.game = Game()
+
+    def test_add_player_card(self):
+        self.game.add_player_card('card')
+        self.assertEqual(self.game.player_cards[0], 'card')
